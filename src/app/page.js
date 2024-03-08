19c8236df07f234861a50277d57ffb66d7efd00d
Fergulati <demetrick@status.im>
2024-03-08 16:59:48 -0500

    create button

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
+
 export default function Home() {
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <p>
-          Hello World!
-        </p>
-      </div>
+      <button>
+        Click me
+      </button>
 
 
 
